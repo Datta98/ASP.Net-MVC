8237b2039c4a707f750434992d4f244987764520
Datta98 <56407906+Datta98@users.noreply.github.com>
2021-12-23 10:14:18 +0530

    DependencyInjection

---
+++ MVC-Examples/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClassLibraryData;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployee _employee = null;
+
+        public HomeController(IEmployee employee)
+        {
+            _employee = employee;
+        }
+        public ActionResult Index()
+        {
+            int count = _employee.GetMembers();            
+            return View();
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
