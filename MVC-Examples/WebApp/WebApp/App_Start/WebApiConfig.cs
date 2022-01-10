7176532755fb07a046662abf0c45e4f6f2962e40
Datta98 <56407906+Datta98@users.noreply.github.com>
2022-01-10 11:21:30 +0530

    WebApp

---
+++ MVC-Examples/WebApp/WebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "BMIAPIRoute",
+                routeTemplate: "api/{controller}/{weight}/{height}",
+                defaults : new {weight=RouteParameter.Optional , height = RouteParameter.Optional}
+                );
+        }
+    }
+}
---
+++ MVC-Examples/WebApp/WebApp/Controllers/BMICalculatorServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class BMICalculatorServiceController : ApiController
+    {
+        //public float Get(float weight,float height)
+        //{
+        //    float bmiValue= BMICalculatorLib.BMICalculatorLib.BMI(weight, height);
+        //    return bmiValue;
+        //}
+        //public float Post([FromBody] Models.BMICalculatorInputData inputData)
+        //{
+        //    float bmiValue = BMICalculatorLib.BMICalculatorLib.BMI(inputData.Weight , inputData.Height);
+        //    return bmiValue;
+        //}
+        //[HttpGet]
+        //public float CalculateBMI(float weight, float height)
+        //{
+        //    float bmiValue = BMICalculatorLib.BMICalculatorLib.BMI(weight, height);
+        //    return bmiValue;
+        //}
+
+        public double GetBMIValue(double weight, double height)
+        {
+            BMIValueCalculationLib.BmiValueCalculation bmiValue1 = new BMIValueCalculationLib.BmiValueCalculation();
+            double bmiValue = bmiValue1.Calculator(height, weight);
+             return bmiValue;
+        }
+    }
+}
